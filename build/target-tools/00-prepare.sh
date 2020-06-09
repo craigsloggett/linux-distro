e9d90e3fad8e7e4f60b9e29642f4b61618de5980
Craig Sloggett <craigsloggett@gmail.com>
2020-06-09 10:31:24 -0400

    Updates

--- build/target-tools/00-prepare.sh
+++ build/target-tools/00-prepare.sh
@@ -7,14 +7,23 @@ BUILD_USER=build
 TARGET_TOOLS_DIR="${CROSS_ROOT_DIR}"/target-tools
 
 # Add the new tooling to the build user's environment.
-printf 'export CC="%s-gcc --sysroot=%s"' "${CROSS_TARGET}" "${TARGET_TOOLS_DIR}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export CXX="%s-g++ --sysroot=%s"' "${CROSS_TARGET}" "${TARGET_TOOLS_DIR}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export LD="%s-ld --sysroot=%s" '"${CROSS_TARGET}" "${TARGET_TOOLS_DIR}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export AR=%s-ar' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export AS=%s-as' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export RANLIB=%s-ranlib' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export READELF=%s-readelf' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
-printf 'export STRIP=%s-strip' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'CC="%s-gcc --sysroot=%s"\n' "${CROSS_TARGET}" "${TARGET_TOOLS_DIR}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'CXX="%s-g++ --sysroot=%s"\n' "${CROSS_TARGET}" "${TARGET_TOOLS_DIR}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'LD="%s-ld --sysroot=%s"\n' "${CROSS_TARGET}" "${TARGET_TOOLS_DIR}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'AR=%s-ar\n' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'AS=%s-as\n' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'RANLIB=%s-ranlib\n' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'READELF=%s-readelf\n' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
+printf 'STRIP=%s-strip\n\n' "${CROSS_TARGET}" >> /home/"${BUILD_USER}"/.bashrc
+
+printf 'export CC\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export CXX\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export LD\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export AR\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export AS\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export RANLIB\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export READELF\n' >> /home/"${BUILD_USER}"/.bashrc
+printf 'export STRIP\n' >> /home/"${BUILD_USER}"/.bashrc
 
 # Prepare the build directories and permissions.
 mkdir -p "${TARGET_TOOLS_DIR}"
